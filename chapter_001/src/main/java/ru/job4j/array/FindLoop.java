904725037545ce3b62d52f65738b1df11df90b8a
xlebyshk <xlebysshk@gmail.com>
2019-12-17 03:48:18 +0300

    Выигрышные комбинации в сокобан[#206361]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -15,9 +15,6 @@ public class FindLoop {
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = start; index < finish;index++) {
-            if (index==finish){
-                break;
-            }
             if (data[index] == el) {
                 rst = index;
                 break;
---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        int  horizontal = 0;
+        int vertical = 0;
+        char sign = ' ';
+        for (int row = 0; row < board.length; row++) {
+            sign = board[row][row];
+            if (sign == 'X') {
+                for (int cell = 0; cell < board.length; cell++) {
+                    sign = board[row][cell];
+                    if (sign == 'X') {
+                        horizontal++;
+                    }
+                    sign = board[cell][row];
+                    if (sign == 'X') {
+                        vertical++;
+                    }
+                }
+                if (vertical == board.length ||  horizontal == board.length) {
+                    result = true;
+                    break;
+                }
+                break;
+            }
+        }
+        return result;
+    }
+}
