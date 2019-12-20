fae815f71544078261e2f6f83fa52e9056e5c2ed
xlebyshk <xlebysshk@gmail.com>
2019-12-20 04:22:50 +0300

    Рефакторинг - Площадь треугольника.[#206497]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,8 +1,37 @@
 package ru.job4j.condition;
 
 public class Triangle {
-
-    public static boolean exist(double ab, double ac, double bc) {
-        return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    private Point first;
+    private Point second;
+    private Point third;
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+    private boolean exist(double a, double c, double b) {
+        return a + b > c && b + c > a && a + c > b;
+    }
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        System.out.println(triangle.area());
     }
 }
+
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,12 +1,18 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 8);
+        Point c = new Point(8, 12);
+        Triangle triangle = new Triangle(a, b, c);
+        double expected = 32;
+        double out = triangle.area();
+        Assert.assertEquals(expected,out,0.01);
     }
 }
