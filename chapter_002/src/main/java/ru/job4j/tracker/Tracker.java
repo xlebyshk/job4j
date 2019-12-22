e71b6b52b78236b654c08b644261c25c3b12af50
xlebyshk <xlebysshk@gmail.com>
2019-12-22 04:28:14 +0300

    Реализовать класс Tracker[#206522]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,61 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    /**
+     * Массив для хранение заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод реализующий добавление заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    public Item[] findAll() {
+        Item[] result = Arrays.copyOf(items, this.position);
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        int count = 0;
+        Item[] result = new Item[this.position];
+        for (int index = 0; index < this.position; index++) {
+            if (items[index].getName().equals(key)) {
+                result[count++] = this.items[index];
+            }
+        }
+        return Arrays.copyOf(result, count);
+    }
+
+    public Item findById(String id) {
+        Item result = null;
+        for (int index = 0; index < this.position; index++) {
+            if (items[index].getId().equals(id)) {
+                result = items[index];
+            }
+        }
+        return result;
+    }
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
 }
