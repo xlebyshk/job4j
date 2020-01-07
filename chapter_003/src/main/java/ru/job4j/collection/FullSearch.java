12e38aed7fb7d51270591c1761832a1aa55030d9
xlebyshk <xlebysshk@gmail.com>
2020-01-07 21:26:38 +0300

    Update

--- chapter_003/src/main/java/ru/job4j/collection/FullSearch.java
+++ chapter_003/src/main/java/ru/job4j/collection/FullSearch.java
@@ -7,9 +7,7 @@ public class FullSearch {
     public static HashSet<String> extractNumber(List<Task> list) {
         HashSet<String> numbers = new HashSet<>();
         for (Task number : list) {
-            if (!numbers.equals(number)) {
-                numbers.add(number.getNumber());
-            }
+            numbers.add(number.getNumber());
         }
         return numbers;
     }
---
+++ chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = false;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String value : origin) {
+            check.add(value);
+        }
+        return rsl;
+    }
+}
