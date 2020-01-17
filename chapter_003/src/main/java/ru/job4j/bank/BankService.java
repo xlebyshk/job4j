43c65efed06fcb6a3c3568f9dd84c4d5df609d30
xlebyshk <xlebysshk@gmail.com>
2020-01-18 01:33:54 +0300

    Тестовое задание из модуля коллекции Lite переделать на Stream API.[#206322]

--- chapter_003/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_003/src/main/java/ru/job4j/bank/BankService.java
@@ -2,16 +2,13 @@ package ru.job4j.bank;
 
 import ru.job4j.collection.Citizen;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-            this.users.putIfAbsent(user, new ArrayList<Account>());
+        this.users.putIfAbsent(user, new ArrayList<Account>());
     }
 
     public void addAccount(String passport, Account account) {
@@ -19,32 +16,18 @@ public class BankService {
     }
 
     public User findByPassport(String passport) {
-        User rsl = null;
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                rsl = user;
-                break;
-            }
-        }
-        return rsl;
+        return
+                this.users.keySet().stream().filter(
+                        user -> user.getPassport().equals(passport)
+                ).findFirst().orElse(null);
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        Account rsl = null;
-        User key;
-        List<Account> accounts;
-        for (Map.Entry<User, List<Account>> entry : users.entrySet()) {
-            key = entry.getKey();
-            accounts = entry.getValue();
-            if (key.getPassport().equals(passport)) {
-                for (Account account : accounts) {
-                    if (account.getRequisite().equals(requisite)) {
-                        rsl = account;
-                    }
-                }
-            }
-        }
-        return rsl;
+        List<Account> accounts = this.users.get(findByPassport(passport));
+        return
+                accounts.stream().filter(
+                        account -> account.getRequisite().equals(requisite)
+                ).findFirst().orElse(null);
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
