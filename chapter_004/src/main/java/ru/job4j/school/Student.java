34190af3158bde0e706c124d4afe7738defcf1b1
xlebyshk <xlebysshk@gmail.com>
2020-01-17 03:42:04 +0300

    Фильтрация учеников.[#206324]

---
+++ chapter_004/src/main/java/ru/job4j/school/Student.java
@@ -0,0 +1,13 @@
+package ru.job4j.school;
+
+public class Student {
+    private final int score;
+
+    public Student(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/school/SchoolTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.school;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SchoolTest {
+
+    @Test
+    public void class10A() {
+        Student student = new Student(45);
+        Student student2 = new Student(84);
+        List<Student> students = Arrays.asList(student, student2);
+        School school = new School();
+        List<Student> rsl = school.collect(students, s -> s.getScore() > 70);
+        assertThat(rsl.get(0), is(student2));
+    }
+
+    @Test
+    public void class10B() {
+        Student student = new Student(66);
+        Student student2 = new Student(71);
+        List<Student> students = Arrays.asList(student, student2);
+        School school = new School();
+        List<Student> rsl = school.collect(students, s -> s.getScore() > 50 && s.getScore() < 70);
+        assertThat(rsl.get(0), is(student));
+    }
+
+    @Test
+    public void class10C() {
+        Student student = new Student(11);
+        Student student2 = new Student(54);
+        List<Student> students = Arrays.asList(student, student2);
+        School school = new School();
+        List<Student> rsl = school.collect(students, s -> s.getScore() < 50);
+        assertThat(rsl.get(0), is(student));
+    }
+
+}
