6c1eb00e3a0938b528cc476fbc71251700c11acd
xlebyshk <xlebysshk@gmail.com>
2020-01-18 06:11:00 +0300

    Local-Variable Type Inference[#206332]

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -10,8 +10,8 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<Person>();
-        for (Person person : persons) {
+        var result = new ArrayList<Person>();
+        for (var person : persons) {
             if (person.getName().contains(key) || person.getSurname().contains(key) || person.getPhone().contains(key) || person.getAddress().contains(key)) {
                 result = persons;
             }
--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -7,7 +7,7 @@ public class PriorityQueue {
 
     public void put(Task task) {
         int index = 0;
-        for (Task element : tasks) {
+        for (var element : tasks) {
             if (task.getPriority() > element.getPriority()) {
                 index++;
                 break;
